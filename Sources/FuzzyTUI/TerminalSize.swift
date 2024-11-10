64d1cdc7aee45126940ec6a8fe2a6c0c406df1b2
Juri Pakaste <juri@juripakaste.fi>
2024-11-10 13:08:55 +0200

    Expose TerminalSize

--- Sources/FuzzyTUI/TerminalSize.swift
+++ Sources/FuzzyTUI/TerminalSize.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-struct TerminalSize {
-    var height: Int
-    var width: Int
+public struct TerminalSize: Sendable, Equatable {
+    public var height: Int
+    public var width: Int
 }
 
 extension TerminalSize {
-    static func current() -> Self {
+    public static func current() -> Self {
         var w = winsize()
         _ = ioctl(STDOUT_FILENO, UInt(TIOCGWINSZ), &w)
         return TerminalSize(height: Int(w.ws_row), width: Int(w.ws_col))
