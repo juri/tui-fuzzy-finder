17c5cb231aa9ca6f2b37ae7d2f452fb8a5e30daf
Juri Pakaste <juri@juripakaste.fi>
2025-10-26 11:46:37 +0200

    Bump terminal-ansi dependency
    For Linux compatibility

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             url: "https://github.com/swift-server/swift-service-lifecycle.git",
             from: "2.6.3"
         ),
-        .package(url: "https://github.com/juri/terminal-ansi", from: "0.1.0"),
+        .package(url: "https://github.com/juri/terminal-ansi", from: "0.3.0"),
     ],
     targets: [
         .target(
